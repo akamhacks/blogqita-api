03cea0983f5606b5c7ce3adaf11ccf9117587e3f
nidzammst <nidzam0501@gmail.com>
2023-03-15 08:41:20 +0700

    edit file index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ const app = require('./app');
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const localhost = process.env.LOCALHOST || '0.0.0.0'
 
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://${localhost}:${port}`);
-  /* eslint-enable no-console */
-});
-
 mongoose.connect(`mongodb+srv://musthafa:${process.env.MONGODB_PASSWORORD}@mern-blog.mhkeity.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-  app.listen(port, localhost)
-  console.log(`Database is connected! Listening to http://${localhost}:${port}`)
-}).catch(err => console.log(err))
+    app.listen(port, localhost)
+    console.log(`Database is connected! Listening to http://${localhost}:${port}`)
+  }).catch(err => console.log(err))
