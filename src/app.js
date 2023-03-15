cd0e1b0820b877df9bf305261adcece6792e8a12
nidzammst <nidzam0501@gmail.com>
2023-03-15 22:50:36 +0700

    nyoba benerin cors issue

--- src/app.js
+++ src/app.js
@@ -13,10 +13,15 @@ const path = require('path')
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'https://blogqita-client.vercel.app' }))
+// app.use(cors({ credentials: true, origin: 'https://blogqita-client.vercel.app' }))
 app.use(bodyParser.json())
 app.use(boolParser())
 app.use(cookieParser())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.get('/', (req, res) => {
   res.json({
