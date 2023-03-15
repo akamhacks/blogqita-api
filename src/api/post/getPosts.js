bfedfcacb6a94d911073f35559951bf9918d43b7
nidzammst <nidzam0501@gmail.com>
2023-03-15 19:41:07 +0700

    memperbaiki masalah query pada fungsi getPosts

--- src/api/post/getPosts.js
+++ src/api/post/getPosts.js
@@ -2,11 +2,11 @@ const Post = require('../../models/Post')
 
 const getPosts = async (req, res, next) => {
     const { page } = req.query
-    const posts = await Post.find(req.query, "-content")
+    const posts = await Post.find({}, "-content")
         .populate('author', ['name'])
         .sort({ count: -1 })
         .limit(20)
-        .skip(page>1 && (page - 1) * 20)
+        .skip(page > 1 && (page - 1) * 20)
     return res.status(200).json(posts)
 }
 
