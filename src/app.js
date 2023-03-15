b5bb8ebd31bd9ee5d72e1de323955301b7cd8a92
nidzammst <nidzam0501@gmail.com>
2023-03-15 23:03:55 +0700

    nyoba benerin cors issue

--- src/app.js
+++ src/app.js
@@ -18,9 +18,38 @@ app.use(bodyParser.json())
 app.use(boolParser())
 app.use(cookieParser())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://blogqita-client.vercel.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+var allowedOrigins = [
+  "https://blogqita-client.vercel.app"
+];
+var origin = req.headers.origin;
+console.log(origin)
+console.log(allowedOrigins.indexOf(origin) > -1)
+// Website you wish to allow to
+if (allowedOrigins.indexOf(origin) > -1) {
+  res.setHeader("Access-Control-Allow-Origin", origin);
+}
+
+// res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+// Request methods you wish to allow
+res.setHeader(
+  "Access-Control-Allow-Methods",
+  "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+);
+
+// Request headers you wish to allow
+res.setHeader(
+  "Access-Control-Allow-Headers",
+  "X-Requested-With,content-type,Authorization"
+);
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+res.setHeader("Access-Control-Allow-Credentials", true);
+
+// Pass to next layer of middleware
+next();
+
 });
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.get('/', (req, res) => {
@@ -31,5 +60,4 @@ app.get('/', (req, res) => {
 app.use('/api/v1', api);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
-
 module.exports = app;
