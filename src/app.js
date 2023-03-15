657fd3775385997d94b06d225796ae5d5507ddab
nidzammst <nidzam0501@gmail.com>
2023-03-15 20:42:22 +0700

    -

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(bodyParser.json())
 app.use(boolParser())
 app.use(cookieParser())
+app.options('*', cors())
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.get('/', (req, res) => {
