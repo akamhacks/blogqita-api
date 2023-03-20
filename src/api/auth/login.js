cd9d7693cbc83218a6abd5d6a81662e27f639ca3
nidzammst <nidzam0501@gmail.com>
2023-03-20 22:21:23 +0700

    '':

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -40,7 +40,8 @@ const login = async (req, res, next) => {
 		expires: new Date(Date.now() + 600000 * 24), // 1 day
 		httpOnly: true,
 		sameSite: "lax",
-		// overwrite: true
+		// overwrite: true,
+		secure: true
 	})
 
 	return res.status(200).json({ message: "Successfully Logged In", firstName: existingUser.firstName, name: existingUser.name, email: existingUser.email, id: existingUser._id, token })
