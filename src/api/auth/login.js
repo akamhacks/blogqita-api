d150b06a0ca15063a92566cad3b3d6f44a36ea7c
nidzammst <nidzam0501@gmail.com>
2023-03-20 20:03:36 +0700

    nyoba hapus path:'/'

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -35,7 +35,13 @@ const login = async (req, res, next) => {
 	}
 
 	console.log("Generated Token\n", token)
-	res.cookie(String(existingUser._id), token)
+	res.cookie(String(existingUser._id), token, {
+		// path: "/",
+		expires: new Date(Date.now() + 600000 * 24), // 1 day
+		httpOnly: true,
+		sameSite: "lax",
+		overwrite: true,
+	})
 
 	return res.status(200).json({ message: "Successfully Logged In", firstName: existingUser.firstName, name: existingUser.name, email: existingUser.email, id: existingUser._id, token })
 }
